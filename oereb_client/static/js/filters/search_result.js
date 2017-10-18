f36e32d57856ddbd59a628fa0b549c1b0e3d549a
Clemens Rudert <clemens.rudert@bl.ch>
2017-10-18 10:29:13 +0200

    provide filter for striping lables of search results a little

---
+++ oereb_client/static/js/filters/search_result.js
@@ -0,0 +1,11 @@
+goog.provide('oereb.searchResultFilter');
+
+goog.require('oereb');
+
+oereb.searchResultFilter = function () {
+  return function(x){
+      return x.replace(/ \((.*?)\)/, '');
+  };
+};
+
+oereb.module.filter('searchResult', oereb.searchResultFilter);
