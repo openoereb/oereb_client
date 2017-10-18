5da1457ade43cc2b6a13c5a6594ee94e97def64e
Clemens Rudert <clemens.rudert@bl.ch>
2017-10-18 16:07:59 +0200

    first approach for search with egrid and combination of parcel number/municipality name

--- oereb_client/static/js/directive/search.js
+++ oereb_client/static/js/directive/search.js
@@ -2,6 +2,9 @@ goog.provide('oereb.searchDirective');
 
 goog.require('oereb');
 goog.require('oereb.SearchService');
+goog.require('oereb.EgridService');
+goog.require('oereb.searchResultFilter');
+goog.require('oereb.ExtractService');
 
 /**
  * Query search API.
@@ -11,13 +14,21 @@ goog.require('oereb.SearchService');
  * @ngdoc directive
  * @ngname oerebSearch
  */
-oereb.searchDirective = function(SearchService, oerebLogoURL, blLogoUrl) {
+oereb.searchDirective = function($filter, SearchService, oerebLogoURL, blLogoUrl, EgridService,
+                                 oerebEventEgridSelected) {
   return {
     restrict: 'E',
     replace: true,
     templateUrl: 'static/html/search.html',
     scope: {},
     link: function(scope, element) {
+      var clear = function () {
+        scope.egrids.length = 0;
+        scope.parcels.length = 0;
+        scope.addresses.length = 0;
+        scope.searchText = undefined;
+      }
+      var filter = $filter('searchResult');
       scope.blLogoUrl = blLogoUrl;
       scope.oerebLogoUrl = oerebLogoURL;
       scope.$watch('searchText', function(value) {
@@ -31,6 +42,29 @@ oereb.searchDirective = function(SearchService, oerebLogoURL, blLogoUrl) {
           scope.parcels = result.features;
         });
       });
+      scope.egridSelect = function(egrid) {
+        clear();
+        scope.$emit(oerebEventEgridSelected, filter(egrid.properties.label));
+      };
+      scope.addressSelect = function (address) {
+        EgridService.getEgridByCoord(address.geometry.coordinates).then(function(egridResponse) {
+          console.log(egridResponse)
+        })
+      };
+      scope.parcelSelect = function (parcel) {
+        var municipality_name = filter(parcel.properties.label).split(' ')[0];
+        var parcel_number = filter(parcel.properties.label).split(' ')[1];
+        SearchService.searchEgrid(parcel_number, municipality_name, ['liegenschaft', 'selbstrecht']).then(
+          function(features) {
+            var result = [];
+            result = result.concat(features[0], features[1]);
+            if (result.length === 1) {
+              clear();
+              scope.$emit(oerebEventEgridSelected, result[0].get('egris_egrid'));
+            }
+          }
+        );
+      };
     }
   }
 };
--- oereb_client/static/js/service/search.js
+++ oereb_client/static/js/service/search.js
@@ -11,13 +11,22 @@ goog.require('oereb');
  * @ngdoc service
  * @ngName SearchService
  */
-oereb.SearchService = function($http, $q, searchServiceConfig) {
+oereb.SearchService = function ($http, $q, searchServiceConfig, wfsFilterServiceUrl) {
 
   this.$http_ = $http;
   this.$q_ = $q;
   var config = angular.fromJson(searchServiceConfig);
-  this.searchServiceUrl_ = config.url;
-  this.limit_ = config.limit
+  this.searchServiceUrl_ = config.api.url;
+  this.searchServiceLimit_ = config.api.limit;
+  this.wfsUrl_ = config.wfs.url;
+  if (this.wfsUrl_.indexOf('?') == -1) {
+    this.wfsUrl_ = this.wfsUrl_ + '?';
+  }
+  this.wfsLimit_ = config.wfs.limit;
+  this.wfsFilterServiceUrl_ = wfsFilterServiceUrl;
+  if (this.wfsFilterServiceUrl_.indexOf('?') == -1) {
+    this.wfsFilterServiceUrl_ = this.wfsFilterServiceUrl_ + '?';
+  }
 
 };
 
@@ -26,17 +35,17 @@ oereb.SearchService = function($http, $q, searchServiceConfig) {
  * @param {String} term The term which is used for searching in the GeoViewBL search API.
  * @returns {angular.$q.Promise} Promise search request.
  */
-oereb.SearchService.prototype.searchTerm = function(term) {
+oereb.SearchService.prototype.searchTerm = function (term) {
   var def = this.$q_.defer();
   var urlItems = [
-      'limit=' + this.limit_,
-      'query=' + term
+    'limit=' + this.searchServiceLimit_,
+    'query=' + term
   ];
   this.$http_.get(this.searchServiceUrl_ + urlItems.join('&')).then(
-    function(response) {
-        def.resolve(response.data);
+    function (response) {
+      def.resolve(response.data);
     },
-    function(response) {
+    function (response) {
       var error = 'Requesting search term failed: ' + term;
       if (angular.isString(response.data)) {
         error += ' ' + response.data;
@@ -45,6 +54,54 @@ oereb.SearchService.prototype.searchTerm = function(term) {
     }
   );
   return def.promise;
+};
+
+/**
+ * Query a WFS for obtaining egrid to combination of parcel number and municipality name.
+ * @param {String} parcel_number The parcel number for the WFS query.
+ * @param {String} municipality_name The municipality name for the WFS query.
+ * @param {Array} layer_names The layer name for the WFS query.
+ * @returns {angular.$q.Promise} Promise search request.
+ */
+oereb.SearchService.prototype.searchEgrid = function (parcel_number, municipality_name, layer_names) {
+  var queue = [];
+  angular.forEach(layer_names, function (layer_name) {
+    var def = this.$q_.defer();
+    var urlItems = [
+      'limit=' + this.wfsLimit_,
+      'layer_name=' + layer_name,
+      'municipality_name=' + municipality_name,
+      'parcel_number=' + parcel_number
+    ];
+    this.$http_.get(this.wfsFilterServiceUrl_ + urlItems.join('&')).then(
+      function (response) {
+        this.$http_({
+          method: "POST",
+          url: this.wfsUrl_,
+          data: response.data
+        }).then(function (wfs_response) {
+          var wfsFormatter = new ol.format.WFS();
+          def.resolve(wfsFormatter.readFeatures(wfs_response.data))
+        }, function (wfs_response) {
+          var error = '';
+          if (angular.isString(wfs_response.data)) {
+            error += ' ' + wfs_response.data;
+          }
+          def.reject(error);
+        })
+        //def.resolve(response.data);
+      }.bind(this),
+      function (response) {
+        var error = '';
+        if (angular.isString(response.data)) {
+          error += ' ' + response.data;
+        }
+        def.reject(error);
+      });
+    queue.push(def.promise);
+  }.bind(this));
+
+  return this.$q_.all(queue);
 };
 
 oereb.module.service('SearchService', oereb.SearchService);
