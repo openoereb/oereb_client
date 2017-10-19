c5cdd34239aa2f429edbb22209caf0c6450ed914
Clemens Rudert <clemens.rudert@bl.ch>
2017-10-19 10:33:08 +0200

    add doc, add switch to recenter map

--- oereb_client/static/js/controller/main.js
+++ oereb_client/static/js/controller/main.js
@@ -8,6 +8,7 @@ goog.require('oereb.MapService');
  * `oereb_client` main controller.
  * @param {angular.Scope} $scope The controller scope.
  * @param {oereb.ExtractService} ExtractService Angular service for extract loading.
+ * @param {oereb.MapService} MapService Angular service for map handling.
  * @param {string} oerebEventEgridSelected Event name for selected EGRID.
  * @param {string} oerebEventExtractLoaded Event name for loaded extract.
  * @constructor
@@ -15,17 +16,24 @@ goog.require('oereb.MapService');
  * @ngdoc controller
  * @ngname MainController
  */
-oereb.MainController = function($scope, ExtractService, oerebEventEgridSelected, oerebEventExtractLoaded) {
+oereb.MainController = function($scope, ExtractService, MapService, oerebEventEgridSelected,
+                                oerebEventExtractLoaded) {
 
   /** @export {boolean} */
   this.extractActive = false;
 
   // Load extract on selected egrid
-  $scope.$on(oerebEventEgridSelected, function(event, egrid) {
+  $scope.$on(oerebEventEgridSelected, function(event, egrid, center) {
     ExtractService.queryExtractById(egrid).then(
       function() {
         $scope.$broadcast(oerebEventExtractLoaded);
         this.extractActive = angular.isDefined(ExtractService.getExtract());
+        if (center) {
+          var geometry = new ol.geom.MultiPolygon(ExtractService.getRealEstate()["Limit"]["coordinates"]);
+          MapService.getMap().getView().fit(geometry, {
+            padding: [0, 0, 0, 500]
+          })
+        }
       }.bind(this),
       function() {
         this.extractActive = false;
