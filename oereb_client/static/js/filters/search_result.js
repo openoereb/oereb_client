d3c8e2299165e62bafee9be9e3620d52281d40c7
Clemens Rudert <clemens.rudert@bl.ch>
2017-10-24 14:08:43 +0200

    add tests for filter, add documentation for filter

--- oereb_client/static/js/filters/search_result.js
+++ oereb_client/static/js/filters/search_result.js
@@ -3,9 +3,18 @@ goog.provide('oereb.searchResultFilter');
 goog.require('oereb');
 
 oereb.searchResultFilter = function () {
-  return function(x) {
-      return x.replace(/ \((.*?)\)/, '');
+  return function(searchResultString) {
+      return searchResultString.replace(/ \((.*?)\)/, '');
   };
 };
 
+/**
+ * @ngdoc filter
+ * @name searchResult
+ * @module oereb
+ *
+ * @description Extracts the information out of the search result string provided by searchAPI.
+ *
+ * @param {string} searchResultString The string which was provided as result by searchAPI.
+ */
 oereb.module.filter('searchResult', oereb.searchResultFilter);
