d6c1859501a6bdf1adceb3bb1dd3955d601922f4
Karsten Deininger <karsten.deininger@bl.ch>
2017-10-26 18:04:01 +0200

    Fix tests

--- test/js/spec/directive/information_panel.spec.js
+++ test/js/spec/directive/information_panel.spec.js
@@ -4,6 +4,7 @@ describe('informationPanelDirective', function() {
 
   beforeEach(angular.mock.module('oereb', function($provide) {
     $provide.constant('oerebApplicationUrl', 'http://example.com');
+    $provide.constant('oerebDefaultLanguage', 'de');
     $provide.constant('oerebBaseLayerConfig', angular.toJson({
       type: 'wms',
       url: 'http://geowms.bl.ch',
@@ -30,6 +31,17 @@ describe('informationPanelDirective', function() {
     });
   }));
 
+  var extract = {
+    PLRCadastreAuthority: 'dummy',
+    CantonalLogoRef: 'dummy',
+    FederalLogoRef: 'dummy',
+    MunicipalityLogoRef: 'dummy'
+  };
+
+  beforeEach(function() {
+    spyOn(ExtractService, 'getExtract').and.returnValue(extract);
+  });
+
   describe('template', function() {
 
     it('should be rendered', function() {
@@ -46,7 +58,7 @@ describe('informationPanelDirective', function() {
       expect(nav.find('strong').eq(2).text()).toContain('Glossar');
       var tabs = element.children('.tab-content');
       expect(tabs.length).toBe(1);
-      expect(tabs.eq(0).children().length).toBe(0);
+      expect(tabs.eq(0).children().length).toBe(1);
       var isoScope = element.isolateScope();
       expect(isoScope.activeTab).toBe(0);
     });
