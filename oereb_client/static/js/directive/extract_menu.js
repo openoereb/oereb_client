68afd11453998b58ded833861a913400c0baef84
Karsten Deininger <karsten.deininger@bl.ch>
2017-10-26 13:51:55 +0000

    Add full legend

--- oereb_client/static/js/directive/extract_menu.js
+++ oereb_client/static/js/directive/extract_menu.js
@@ -3,6 +3,16 @@ goog.provide('oereb.extractMenuDirective');
 goog.require('oereb');
 goog.require('oereb.staticExtractDirective');
 
+/**
+ * Drirective definition function.
+ *
+ * @param {angular.$location} $location Angular $location service.
+ * @param {string} geoViewConfig JSON-encoded GeoView BL configuration.
+ *
+ * @returns {angular.Directive} Angular directive definition.
+ *
+ * @ngInject
+ */
 oereb.extractMenuDirective = function ($location, geoViewConfig) {
   return {
     restrict: 'E',
@@ -12,10 +22,21 @@ oereb.extractMenuDirective = function ($location, geoViewConfig) {
 
       /** @export */
       permaLink: '='
+
     },
-    link: function(scope) {
+    link: function(scope, element) {
+
+      // Initialize tooltip
+      element.children('button').tooltip();
+
+      /** @export {string} */
       scope.permaLink = '';
-      scope.goToGeoView = function () {
+
+      /**
+       * Switch to GeoView BL at the currently selected EGRID.
+       * @export
+       */
+      scope.goToGeoView = function() {
         var egrid = $location.search()['egrid'];
         var url = angular.fromJson(geoViewConfig).url;
         var parameters = [
@@ -28,21 +49,28 @@ oereb.extractMenuDirective = function ($location, geoViewConfig) {
           '_blank'
         );
       };
-      scope.providePermalink = function () {
+
+      /**
+       * Show the permalink in a modal window.
+       * @export
+       */
+      scope.providePermalink = function() {
         scope.permaLink = $location.absUrl();
-        $('#modal-permalink').modal('toggle');
-      }
+        angular.element('#modal-permalink').modal('toggle');
+      };
+
     }
   }
 };
 
 /**
  * @ngdoc directive
- * @name extractMenu
+ * @name oerebExtractMenu
  * @module oereb
  * @restrict E
  *
  * @description The menu which is shown in the extract to link to several related services.
  *
+ * @param {string} permaLink The permalink variable.
  */
 oereb.module.directive('oerebExtractMenu', oereb.extractMenuDirective);
--- test/js/spec/directive/legend.spec.js
+++ test/js/spec/directive/legend.spec.js
@@ -7,7 +7,7 @@ describe('legendDirective', function() {
     $provide.constant('oerebDefaultLanguage', 'de');
   }));
 
-  var $compile, $rootScope, ExtractService;
+  var $compile, $rootScope, $timeout, ExtractService;
 
   var legendEntries = [
     {
@@ -30,14 +30,20 @@ describe('legendDirective', function() {
     }
   ];
 
-  beforeEach(inject(function(_$compile_, _$rootScope_, _ExtractService_) {
+  var legendGraphics = ['http://example.com/wms?SERVICE=WMS&REQUEST=GetLegendGraphic&LAYERS=test'];
+
+  beforeEach(inject(function(_$compile_, _$rootScope_, _$timeout_, _ExtractService_) {
     $compile = _$compile_;
     $rootScope = _$rootScope_;
+    $timeout = _$timeout_;
     ExtractService = _ExtractService_;
   }));
 
   beforeEach(function () {
-    spyOn(ExtractService, 'getLegend').and.returnValue(legendEntries);
+    spyOn(ExtractService, 'getLegend').and.returnValue({
+      'entries': legendEntries,
+      'graphics': legendGraphics
+    });
   });
 
   describe('template', function() {
@@ -50,9 +56,10 @@ describe('legendDirective', function() {
       $rootScope.$digest();
       var scope = element.isolateScope();
       expect(ExtractService.getLegend).toHaveBeenCalledWith('test');
-      expect(scope.legendEntries.length).toBe(2);
+      expect(scope.legend['entries'].length).toBe(2);
+      expect(scope.legend['graphics'].length).toBe(1);
       var rows = element.find('tr');
-      expect(rows.length).toBe(3);
+      expect(rows.length).toBe(4);
       expect(rows.eq(0).find('small').length).toBe(3);
       expect(rows.eq(1).children('td').eq(0).text()).toContain(legendEntries[0].Information[0].Text);
       expect(rows.eq(1).children('td').eq(2).text()).toContain(legendEntries[0].Area);
@@ -60,6 +67,40 @@ describe('legendDirective', function() {
       expect(rows.eq(2).children('td').eq(0).text()).toContain(legendEntries[1].Information[0].Text);
       expect(rows.eq(2).children('td').eq(2).text()).toContain(legendEntries[1].Area);
       expect(rows.eq(2).children('td').eq(3).text()).toContain(legendEntries[1].PartInPercent);
+      expect(rows.eq(3).find('img').eq(0).attr('src')).toContain(legendGraphics[0]);
+    });
+
+  });
+
+  describe('toggleGraphics', function() {
+
+    it('should show/hide the legend graphics', function(done) {
+      $rootScope.themeCode = 'test';
+      var element = $compile(
+        '<oereb-legend theme-code="::themeCode"></oereb-legend>'
+      )($rootScope);
+      $rootScope.$digest();
+      var scope = element.isolateScope();
+      var button = element.find('tr').eq(3).find('span').first();
+      var collapsible = element.find('.collapse').first();
+      expect(button.find('small').first().text()).toContain('vollständige Legende anzeigen');
+      expect(button.find('i').first().hasClass('fa-chevron-down')).toBe(true);
+      expect(collapsible.hasClass('in')).toBe(false);
+      scope.toggleGraphics();
+      $timeout.flush();
+      setTimeout(function() {
+        expect(button.find('small').first().text()).toContain('vollständige Legende verbergen');
+        expect(button.find('i').first().hasClass('fa-chevron-up')).toBe(true);
+        expect(collapsible.hasClass('in')).toBe(true);
+        scope.toggleGraphics();
+        $timeout.flush();
+        setTimeout(function() {
+          expect(button.find('small').first().text()).toContain('vollständige Legende anzeigen');
+          expect(button.find('i').first().hasClass('fa-chevron-down')).toBe(true);
+          expect(collapsible.hasClass('in')).toBe(false);
+          done();
+        }, 600);
+      }, 600);
     });
 
   });
