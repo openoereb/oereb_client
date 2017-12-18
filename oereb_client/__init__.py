4911de4b5feecc3b0e2f5c87182d33e372061a07
Karsten Deininger <karsten.deininger@bl.ch>
2017-12-18 08:54:43 +0100

    Increase beta version

--- oereb_client/__init__.py
+++ oereb_client/__init__.py
@@ -4,7 +4,7 @@ from pyramid.config import Configurator
 from pyramid_mako import add_mako_renderer
 
 
-__version__ = '1.0.0-beta.1'
+__version__ = '1.0.0-beta.2'
 
 
 def main(global_config, **settings):  # pragma: no cover
