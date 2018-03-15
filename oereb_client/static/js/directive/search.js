3caf505c4f9c0502f31e8d2d86e73582fcd29b2f
Karsten Deininger <karsten.deininger@bl.ch>
2018-03-15 14:12:10 +0100

    Enable search for coordinates

--- oereb_client/static/js/directive/search.js
+++ oereb_client/static/js/directive/search.js
@@ -5,6 +5,7 @@ goog.require('oereb.SearchService');
 goog.require('oereb.EgridService');
 goog.require('oereb.searchResultFilter');
 goog.require('oereb.ExtractService');
+goog.require('oereb.mapQueryDirective');
 
 /**
  * Query search API.
@@ -39,6 +40,10 @@ oereb.searchDirective = function($filter, SearchService, EgridService, oerebLogo
 
       /** @export {String} */
       scope.searchText = '';
+
+      var reLV03 = new RegExp('^(\\d{6}(\\.\\d+)?)(\\s|,\\s?|;\\s?)(\\d{6}(\\.\\d+)?)');
+      var reLV95 = new RegExp('^(\\d{7}(\\.\\d+)?)(\\s|,\\s?|;\\s?)(\\d{7}(\\.\\d+)?)');
+      var reGNSS = new RegExp('^(\\d{1}(\\.\\d+)?)(\\s|,\\s?|;\\s?)(\\d{2}(\\.\\d+)?)');
 
       var egrLoading = false;
       var adrLoading = false;
@@ -80,8 +85,77 @@ oereb.searchDirective = function($filter, SearchService, EgridService, oerebLogo
             scope.parcels = result.features;
           }, function() {});
         }
-
       });
+
+      /**
+       * Return true, if the search text is a valid LV03 coordinate.
+       * @returns {boolean} True, if the search text is a valid LV03 coordinate.
+       * @export
+       */
+      scope.isLV03 = function() {
+        return reLV03.test(scope.searchText);
+      };
+
+      /**
+       * Return true, if the search text is a valid LV95 coordinate.
+       * @returns {boolean} True, if the search text is a valid LV95 coordinate.
+       * @export
+       */
+      scope.isLV95 = function() {
+        return reLV95.test(scope.searchText);
+      };
+
+      /**
+       * Return true, if the search text is a valid WGS84 coordinate.
+       * @returns {boolean} True, if the search text is a valid WGS84 coordinate.
+       * @export
+       */
+      scope.isGNSS = function() {
+        return reGNSS.test(scope.searchText);
+      };
+
+      /**
+       * Returns the matched coordinate.
+       * @param {RegExpExecArray|null} match The coordinate match.
+       * @returns {ol.Coordinate|null} The matched coordinate.
+       * @private
+       */
+      scope.parseCoordinateMatch_ = function(match) {
+        if (angular.isArray(match)) {
+          return [
+            parseFloat(match[1]),
+            parseFloat(match[4])
+          ]
+        }
+        return null;
+      };
+
+      /**
+       * Returns the LV03 match.
+       * @returns {ol.Coordinate|null} The matched coordinate.
+       * @export
+       */
+      scope.getLV03 = function() {
+        return scope.parseCoordinateMatch_(reLV03.exec(scope.searchText));
+      };
+
+      /**
+       * Returns the LV95 match.
+       * @returns {ol.Coordinate|null} The matched coordinate.
+       * @export
+       */
+      scope.getLV95 = function() {
+        return scope.parseCoordinateMatch_(reLV95.exec(scope.searchText));
+      };
+
+      /**
+       * Returns the WGS84 match.
+       * @returns {ol.Coordinate|null} The matched coordinate.
+       * @export
+       */
+      scope.getGNSS = function() {
+        return scope.parseCoordinateMatch_(reGNSS.exec(scope.searchText));
+      };
 
       /**
        * The function which is used if user selects EGRID out of search results.
@@ -103,6 +177,29 @@ oereb.searchDirective = function($filter, SearchService, EgridService, oerebLogo
         var selectorScope = selector['isolateScope']();
         clear();
         selectorScope.queryAt(address["geometry"]["coordinates"], true);
+      };
+
+      /**
+       * Queries the real estate at the matched coordinates.
+       * @param {ol.Coordinate|null} coordinate The matched coordinate.
+       * @export
+       */
+      scope.coordinateSelect = function(coordinate) {
+        if (coordinate !== null) {
+          var lv95Coordinate = coordinate;
+          var selector = angular.element('#map-query');
+          var selectorScope = selector['isolateScope']();
+          if (scope.isLV03()) {
+            lv95Coordinate = new ol.geom.Point(coordinate).transform('EPSG:21781', 'EPSG:2056')
+              .getCoordinates();
+          }
+          else if (scope.isGNSS()) {
+            lv95Coordinate = new ol.geom.Point(coordinate).transform('EPSG:4326', 'EPSG:2056')
+              .getCoordinates();
+          }
+          clear();
+          selectorScope.queryAt(lv95Coordinate, true);
+        }
       };
 
       /**
