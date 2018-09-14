5f4f623e2ceda2a2796506c20e8bb52334d5ff29
Karsten Deininger <karsten.deininger@bl.ch>
2018-09-14 08:35:20 +0200

    Improve glossary sorting

--- test/js/spec/filters/sort_glossary.spec.js
+++ test/js/spec/filters/sort_glossary.spec.js
@@ -28,7 +28,7 @@ describe('sortGlossaryFilter', function() {
         ],
         Content: [
           {
-            Text: 'four',
+            Text: 'five',
             Language: 'de'
           }
         ]
@@ -56,7 +56,7 @@ describe('sortGlossaryFilter', function() {
         ],
         Content: [
           {
-            Text: 'two',
+            Text: 'three',
             Language: 'de'
           }
         ]
@@ -70,7 +70,88 @@ describe('sortGlossaryFilter', function() {
         ],
         Content: [
           {
+            Text: 'four',
+            Language: 'de'
+          }
+        ]
+      },
+      {
+        Title: [
+          {
+            Text: 'ÄÄÄ',
+            Language: 'de'
+          }
+        ],
+        Content: [
+          {
+            Text: 'two',
+            Language: 'de'
+          }
+        ]
+      }
+    ];
+    var sorted = $filter('sortGlossary')(elements);
+    expect(sorted[0].Content[0].Text).toEqual('one');
+    expect(sorted[1].Content[0].Text).toEqual('two');
+    expect(sorted[2].Content[0].Text).toEqual('three');
+    expect(sorted[3].Content[0].Text).toEqual('four');
+    expect(sorted[4].Content[0].Text).toEqual('five');
+  });
+
+  it('should return the sorted non-string elements', function() {
+    var elements = [
+      {
+        Title: [
+          {
+            Text: 3,
+            Language: 'de'
+          }
+        ],
+        Content: [
+          {
             Text: 'three',
+            Language: 'de'
+          }
+        ]
+      },
+      {
+        Title: [
+          {
+            Text: 3,
+            Language: 'de'
+          }
+        ],
+        Content: [
+          {
+            Text: 'four',
+            Language: 'de'
+          }
+        ]
+      },
+      {
+        Title: [
+          {
+            Text: 1,
+            Language: 'de'
+          }
+        ],
+        Content: [
+          {
+            Text: 'one',
+            Language: 'de'
+          }
+        ]
+      },
+      {
+        Title: [
+          {
+            Text: 2,
+            Language: 'de'
+          }
+        ],
+        Content: [
+          {
+            Text: 'two',
             Language: 'de'
           }
         ]
