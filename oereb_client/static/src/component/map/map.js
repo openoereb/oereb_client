b4f1333b14dd138a562c7ce17e31fa20efbc2eae
Karsten Deininger <karsten.deininger@bl.ch>
2021-07-20 15:29:39 +0000

    Update URL parameters

--- oereb_client/static/src/component/map/map.js
+++ oereb_client/static/src/component/map/map.js
@@ -1,26 +1,44 @@
 import './map.scss';
 import OerebView from '../view/view';
+import OerebBaseLayer from '../base_layer/base_layer';
 import React from 'react';
 import Map from 'ol/Map';
+import {defaults} from 'ol/control';
 
 class OerebMap extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            map: new Map()
+            map: new Map({
+                controls: defaults({
+                    attribution: false
+                })
+            })
         }
     }
 
     componentDidMount() {
         this.state.map.setTarget(this._element);
+        this.state.map.on('moveend', function() {
+            this.updateUrlParams();
+        }.bind(this));
     }
 
     render() {
         return (
             <div ref={el => (this._element = el)} class="oereb-client-map">
                 <OerebView map={this.state.map} />
+                <OerebBaseLayer map={this.state.map} />
             </div>
         );
+    }
+
+    updateUrlParams() {
+        const query = new URLSearchParams(window.location.search);
+        query.set('map_x', this.state.map.getView().getCenter()[0].toFixed(3));
+        query.set('map_y', this.state.map.getView().getCenter()[1].toFixed(3));
+        query.set('map_zoom', this.state.map.getView().getZoom().toFixed(0));
+        window.history.pushState(null, null, '?' + query.toString());
     }
 }
 
--- oereb_client/static/src/component/view/view.js
+++ oereb_client/static/src/component/view/view.js
@@ -1,17 +1,14 @@
 import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import TileWMS from 'ol/source/TileWMS';
-import WMTS, { optionsFromCapabilities } from 'ol/source/WMTS';
-import WMTSCapabilities from 'ol/format/WMTSCapabilities';
 import { useSelector } from 'react-redux';
 
 function OerebView(props) {
     const config = useSelector((state) => state.config).config;
+    const query = new URLSearchParams(window.location.search);
 
     // Add view
-    const mapX = config.view.map_x;
-    const mapY = config.view.map_y;
-    const mapZoom = config.view.map_zoom;
+    const mapX = parseFloat(query.get('map_x')) || config.view.map_x;
+    const mapY = parseFloat(query.get('map_y')) || config.view.map_y;
+    const mapZoom = parseFloat(query.get('map_zoom')) || config.view.map_zoom;
     const view = new View({
         center: [mapX, mapY],
         zoom: mapZoom,
@@ -20,60 +17,7 @@ function OerebView(props) {
     });
     props.map.setView(view);
 
-    // Add base layer
-    getBaseLayerSource(config['base_layer']).then(function(source) {
-        const baseLayer = new TileLayer({
-            preload: Infinity,
-            visible: true,
-            source: source
-        });
-        props.map.addLayer(baseLayer);
-    });
-
-    console.log(config);
     return null;
-}
-
-function getBaseLayerSource(config) {
-    if (config['type'].toLowerCase() === 'wms') {
-        return getBaseLayerSourceWms(config);
-    }
-    else if (config['type'].toLowerCase() === 'wmts') {
-        return getBaseLayerSourceWmts(config);
-    }
-    else {
-        return Promise.reject('Invalid base layer type');
-    }
-}
-
-function getBaseLayerSourceWms(config) {
-    return Promise.resolve(new TileWMS({
-        url: config['url'],
-        params: config['params'],
-        projection: 'EPSG:2056'
-    }));
-}
-
-function getBaseLayerSourceWmts(config) {
-    const parser = new WMTSCapabilities();
-    return new Promise(function(resolve, reject) {
-        fetch(config['url'])
-        .then(response => response.text())
-        .then((xml) => {
-            const wmtsCaps = parser.read(xml);
-            let wmtsOptions = {};
-            Object.entries(config).forEach(([key, value]) => {
-                if (key !== 'url') {
-                    wmtsOptions[key] = value;
-                }
-            });
-            const wmtsConfig = optionsFromCapabilities(wmtsCaps, wmtsOptions);
-            resolve(new WMTS(wmtsConfig));
-        })
-        .catch((error) => {
-            reject(error);
-        });
-    });
 }
 
 export default OerebView;
