fa9e0c11e586333c86b09e5e2bfc2e46f6ae1ecf
Karsten Deininger <karsten.deininger@bl.ch>
2021-09-24 07:52:48 +0000

    Reload extract on changed language

--- oereb_client/static/src/api/extract.js
+++ oereb_client/static/src/api/extract.js
@@ -1,12 +1,18 @@
-export const queryExtractById = function (applicationUrl, egrid) {
+export const queryExtractById = function (applicationUrl, egrid, language) {
   const url = new URL(applicationUrl + 'extract/reduced/json/geometry/' + egrid);
   url.searchParams.append('_dc', new Date().getTime());
+  if (language) {
+    url.searchParams.append('LANG', language);
+  }
   return fetch(url).then((response) => response.json());
 };
 
-export const queryStaticExtractById = function (applicationUrl, egrid) {
+export const queryStaticExtractById = function (applicationUrl, egrid, language) {
   const url = new URL(applicationUrl + 'extract/reduced/pdf/' + egrid);
   url.searchParams.append('_dc', new Date().getTime());
+  if (language) {
+    url.searchParams.append('LANG', language);
+  }
   return fetch(url).then((response) => response.arrayBuffer());
 };
 
