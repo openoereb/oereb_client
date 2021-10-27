275e76fa1e6fb7cd31a096868e50b7be25d75829
Karsten Deininger <karsten.deininger@bl.ch>
2021-10-27 09:15:26 +0000

    Split topics by lawstatus

--- oereb_client/static/src/component/topic_list/topics_with_restrictions.js
+++ oereb_client/static/src/component/topic_list/topics_with_restrictions.js
@@ -1,18 +1,43 @@
 import PropTypes from 'prop-types';
 import React, {useRef} from 'react';
+import {useSelector} from 'react-redux';
 
 import {sanitizeTopicCode} from '../../api/extract';
+import {getLocalizedText} from '../../util/language';
 import OerebTopic from '../topic/topic';
 
 const OerebTopicsWithRestriction = function (props) {
   const topics = props.data;
   const restrictions = props.restrictions;
   const accordionEl = useRef(null);
+  const language = useSelector((state) => state.language);
+  const currentLanguage = language.current;
+  const defaultLanguage = language.default;
 
   const topicList = topics.map((topic, key) => {
     const code = sanitizeTopicCode(topic.Code);
+    const title = getLocalizedText(topic.Text, currentLanguage, defaultLanguage);
+    let inForce = null;
+    let changeWithPreEffect = null;
+    let changeWithoutPreEffect = null;
+    if (restrictions[code].inForce.length > 0) {
+      inForce = <OerebTopic restrictions={restrictions[code].inForce} />;
+    }
+    if (restrictions[code].changeWithPreEffect.length > 0) {
+      changeWithPreEffect = <OerebTopic restrictions={restrictions[code].changeWithPreEffect} />;
+    }
+    if (restrictions[code].changeWithoutPreEffect.length > 0) {
+      changeWithoutPreEffect = <OerebTopic restrictions={restrictions[code].changeWithoutPreEffect} />;
+    }
     return (
-      <OerebTopic key={key} topic={topic} restrictions={restrictions[code]} />
+      <div className="accordion-item" key={key}>
+        <div className="accordion-item p-2 ps-3">
+          {title}
+        </div>
+        {inForce}
+        {changeWithPreEffect}
+        {changeWithoutPreEffect}
+      </div>
     );
   });
 
