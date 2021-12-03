34cfca6e2df943a6576342a480f8343945fae53b
Karsten Deininger <karsten.deininger@bl.ch>
2021-12-03 05:35:04 +0000

    Add story for documents

---
+++ oereb_client/static/src/component/documents/documents.stories.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import data from "../../../../../samples/extract.json";
+import { groupRestrictionsByTopic } from "../../request/extract";
+import OerebDocuments from "./documents";
+
+export default {
+  title: 'API Reference/Component/Documents',
+  component: OerebDocuments
+};
+
+export const Documents = () => {
+  const restrictions = groupRestrictionsByTopic(
+    data.GetExtractByIdResponse.extract.RealEstate.RestrictionOnLandownership
+  )['chBelasteteStandorteOeffentlicherVerkehr']['changeWithPreEffect'];
+  return <OerebDocuments restrictions={restrictions} />
+};
