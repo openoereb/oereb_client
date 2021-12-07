d7a066cc4586486abb60943072babfba453ec81b
Karsten Deininger <karsten.deininger@bl.ch>
2021-12-07 08:24:20 +0000

    Add story for external viewer button

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,26 @@
 import {Provider} from 'react-redux';
 import {update} from '../oereb_client/static/src/reducer/config';
-import {themes} from '@storybook/theming';
+import {initLanguages} from '../oereb_client/static/src/reducer/language';
 import MainStore from '../oereb_client/static/src/store/main';
 import config from './oereb_client.json';
 import OerebTheme from './theme';
+import {useDispatch} from 'react-redux';
 
 export const decorators = [
   (Story) => {
-    let cfg = config['oereb_client'];
-    cfg['application_url'] = 'https://example.com';
-    update(cfg);
+    const dispatch = useDispatch();
+    const cfg = Object.assign({
+      application_url: 'https://example.com',
+      service_url: 'https://example.com'
+    }, config['oereb_client']);
+    dispatch(update(cfg));
+    dispatch(initLanguages({
+      available: cfg['application']['languages'],
+      default: cfg['application']['default_language']
+    }));
+    return <Story />;
+  },
+  (Story) => {
     return (
       <Provider store={MainStore}>
         <Story />
