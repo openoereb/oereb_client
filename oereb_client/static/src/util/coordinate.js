5149f5e1602b1e6c9b04b638148160d8cb7a1fa7
Karsten Deininger <karsten.deininger@bl.ch>
2022-01-19 10:59:27 +0000

    Remove support for LV03 coordinates

--- oereb_client/static/src/util/coordinate.js
+++ oereb_client/static/src/util/coordinate.js
@@ -1,7 +1,6 @@
 import {isArray} from "lodash";
 import Point from "ol/geom/Point";
 
-const reLV03 = new RegExp('^(\\d{6}(\\.\\d+)?)(\\s|,\\s?|;\\s?)(\\d{6}(\\.\\d+)?)');
 const reLV95 = new RegExp('^(\\d{7}(\\.\\d+)?)(\\s|,\\s?|;\\s?)(\\d{7}(\\.\\d+)?)');
 const reGNSS = new RegExp('^(\\d{1}(\\.\\d+)?)(\\s|,\\s?|;\\s?)(\\d{2}(\\.\\d+)?)');
 
@@ -13,10 +12,6 @@ const parseCoordinateMatch = function(match) {
     ]
   }
   return null;
-};
-
-export const isLV03 = function(value) {
-  return reLV03.test(value);
 };
 
 export const isLV95 = function(value) {
@@ -31,10 +26,6 @@ export const getCoordinates = function(value) {
   if (isGNSS(value)) {
     const coords = parseCoordinateMatch(reGNSS.exec(value));
     return new Point(coords).transform('EPSG:4326', 'EPSG:2056').getCoordinates();
-  }
-  else if (isLV03(value)) {
-    const coords = parseCoordinateMatch(reLV03.exec(value));
-    return new Point(coords).transform('EPSG:21781', 'EPSG:2056').getCoordinates();
   }
   else if (isLV95(value)) {
     return parseCoordinateMatch(reLV95.exec(value));
