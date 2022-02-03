ce92b562660986953e932c880a49073ef1246f95
Karsten Deininger <karsten.deininger@bl.ch>
2022-02-03 08:07:41 +0000

    Add tests for egrid request

--- oereb_client/static/src/request/egrid.js
+++ oereb_client/static/src/request/egrid.js
@@ -2,5 +2,5 @@ export const queryEgridByCoord = function (serviceUrl, coord) {
   const url = new URL(serviceUrl + 'getegrid/json/');
   url.searchParams.append('EN', '' + coord[0] + ',' + coord[1]);
   url.searchParams.append('_dc', new Date().getTime());
-  return fetch(url).then((response) => response.json());
+  return fetch(url.toString()).then((response) => response.json());
 };
