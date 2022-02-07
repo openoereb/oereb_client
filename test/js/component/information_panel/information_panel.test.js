cec79b564deb5465a1d88073a9f84bcfae590f17
Karsten Deininger <karsten.deininger@bl.ch>
2022-02-07 05:56:31 +0000

    Add tests for information panel component

---
+++ test/js/component/information_panel/information_panel.test.js
@@ -0,0 +1,67 @@
+import {mount} from "enzyme";
+import toJson from "enzyme-to-json";
+import React from "react";
+import {Provider} from "react-redux";
+
+import OerebInformationPanel
+  from "../../../../oereb_client/static/src/component/information_panel/information_panel";
+import {
+  loadExtract,
+  setInformationPanelTab,
+  showExtract
+} from "../../../../oereb_client/static/src/reducer/extract";
+import {initLanguages} from "../../../../oereb_client/static/src/reducer/language";
+import MainStore from "../../../../oereb_client/static/src/store/main";
+import extract from "../../../../samples/extract.json";
+
+beforeEach(() => {
+  MainStore.dispatch(initLanguages({
+    default: 'de',
+    available: ['de']
+  }));
+  MainStore.dispatch(loadExtract({
+    egrid: 'CH1234',
+    zoom: false
+  }));
+  MainStore.dispatch(showExtract(extract));
+});
+
+describe('information panel component', () => {
+
+  let component;
+
+  beforeEach(() => {
+    component = mount(
+      <Provider store={MainStore}>
+        <OerebInformationPanel />
+      </Provider>
+    );
+  });
+
+  it('should render general information', () => {
+    MainStore.dispatch(setInformationPanelTab(0));
+    component.update();
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should render disclaimer', () => {
+    MainStore.dispatch(setInformationPanelTab(1));
+    component.update();
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should render glossary', () => {
+    MainStore.dispatch(setInformationPanelTab(2));
+    component.update();
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should filter glossary results', () => {
+    MainStore.dispatch(setInformationPanelTab(2));
+    component.update();
+    component.find('input').simulate('change', {target: {value: 'kbs'}});
+    component.update();
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+});
