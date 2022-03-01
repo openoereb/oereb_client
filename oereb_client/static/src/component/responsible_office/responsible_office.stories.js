31059de0b8b502959f4cf24cb2e80e2f5881ffe2
Karsten Deininger <karsten.deininger@bl.ch>
2022-03-01 13:55:10 +0000

    Fix documentation for components

--- oereb_client/static/src/component/responsible_office/responsible_office.stories.js
+++ oereb_client/static/src/component/responsible_office/responsible_office.stories.js
@@ -11,7 +11,8 @@ export default {
 
 export const ResponsibleOffice = () => {
   const restrictions = groupRestrictionsByTopic(
-    data.GetExtractByIdResponse.extract.RealEstate.RestrictionOnLandownership
+    data.GetExtractByIdResponse.extract.RealEstate.RestrictionOnLandownership,
+    data.GetExtractByIdResponse.extract.ConcernedTheme
   )['chBelasteteStandorteOeffentlicherVerkehr']['changeWithPreEffect'];
   return <OerebResponsibleOffice restrictions={restrictions} />
 };
