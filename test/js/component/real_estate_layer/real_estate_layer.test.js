9376ad14f74738c9d3bc073193fd7ea2076d4ab2
Karsten Deininger <karsten.deininger@bl.ch>
2022-03-04 08:35:03 +0000

    Add test for real estate layer component

---
+++ test/js/component/real_estate_layer/real_estate_layer.test.js
@@ -0,0 +1,46 @@
+import {mount} from "enzyme";
+import toJson from "enzyme-to-json";
+import {Map} from "ol";
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from "ol/source/Vector";
+import React from "react";
+import {act} from "react-dom/test-utils";
+import {Provider} from "react-redux";
+
+import OerebRealEstateLayer from
+  "../../../../oereb_client/static/src/component/real_estate_layer/real_estate_layer";
+import {loadExtract, showExtract} from "../../../../oereb_client/static/src/reducer/extract";
+import MainStore from "../../../../oereb_client/static/src/store/main";
+import extract from "../../../../samples/extract.json";
+
+describe('real estate layer component', () => {
+
+  let component;
+
+  beforeEach(() => {
+    const map = new Map();
+    const layer = new VectorLayer({
+      source: new VectorSource()
+    });
+    map.addLayer(layer);
+    act(() => {
+      MainStore.dispatch(loadExtract({
+        egrid: 'CH1234',
+        zoom: false
+      }));
+    });
+    act(() => {
+      MainStore.dispatch(showExtract(extract));
+    });
+    component = mount(
+      <Provider store={MainStore}>
+        <OerebRealEstateLayer realEstateLayer={layer} map={map} />
+      </Provider>
+    );
+  });
+
+  it('should render real estate layer element', () => {
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+});
