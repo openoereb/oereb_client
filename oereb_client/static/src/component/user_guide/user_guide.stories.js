e87f96e7d495857fb4f6d0d7e9686fad70e5533a
Karsten Deininger <karsten.deininger@bl.ch>
2022-04-07 12:24:28 +0000

    Add documentation

---
+++ oereb_client/static/src/component/user_guide/user_guide.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import OerebUserGuide from "./user_guide";
+
+export default {
+  title: 'API Reference/Component/User Guide',
+  component: OerebUserGuide
+};
+
+export const UserGuide = () => <OerebUserGuide />;
+UserGuide.title = 'User Guide';
