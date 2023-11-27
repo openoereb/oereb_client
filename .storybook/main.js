f789f918279a1b52b07b5a14d8e0bf5a67247381
Karsten Deininger <karsten.deininger@bl.ch>
2023-11-27 15:10:31 +0000

    Use Vite instead of Webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,12 @@
+import { mergeConfig } from 'vite';
 import remarkGfm from 'remark-gfm';
-const path = require('path');
-module.exports = {
-  "stories": ["../docs/src/**/*.stories.mdx", "../oereb_client/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
-  "addons": [
+
+export default {
+  stories: [
+    '../docs/src/**/*.stories.mdx',
+    '../oereb_client/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+  ],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "storybook-react-i18next",
@@ -17,21 +21,43 @@ module.exports = {
       }
     }
   ],
-  "webpackFinal": async (config, {
-    configType
-  }) => {
-    config.module.rules.push({
-      test: /\.s[ac]ss$/i,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../')
-    });
-    return config;
-  },
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: {}
-  },
   docs: {
     autodocs: true
-  }
-};
+  },
+  framework: '@storybook/react-vite',
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      // Add storybook-specific dependencies to pre-optimization
+      optimizeDeps: {
+        include: ['storybook-addon-docs'],
+      },
+    });
+  },
+ };
+
+// module.exports = {
+//   stories: ["../docs/src/**/*.stories.mdx", "../oereb_client/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
+//   core: {
+//     builder: '@storybook/builder-vite'
+//   },
+//   addons: [
+//     "@storybook/addon-links",
+//     "@storybook/addon-essentials",
+//     "storybook-react-i18next",
+//     {
+//       name: '@storybook/addon-docs',
+//       options: {
+//         mdxPluginOptions: {
+//           mdxCompileOptions: {
+//             remarkPlugins: [remarkGfm],
+//           }
+//         }
+//       }
+//     }
+//   ],
+//   framework: '@storybook/react-vite',
+//   docs: {
+//     autodocs: true
+//   }
+// };
---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import htmlPurge from 'vite-plugin-purgecss';
+
+export default defineConfig({
+  build: {
+    outDir: path.resolve(__dirname, 'oereb_client/static/build'),
+    lib: {
+      entry: path.resolve(__dirname, 'oereb_client/static/src/oereb_client.jsx'),
+      name: "app",
+      fileName: "app"
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: "app.[ext]"
+      },
+    }
+  },
+  plugins: [
+    react(),
+    htmlPurge()
+  ],
+  define: {
+    'process.env.NODE_ENV': '"production"'
+  }
+});
