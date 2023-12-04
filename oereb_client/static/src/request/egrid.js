ef9cbca6378321e1d4d4fd22849fc090c9dedfad
Karsten Deininger <karsten.deininger@bl.ch>
2023-12-04 10:38:44 +0000

    Improve error handling in getegrid request

--- oereb_client/static/src/request/egrid.js
+++ oereb_client/static/src/request/egrid.js
@@ -2,5 +2,14 @@ export const queryEgridByCoord = function (serviceUrl, coord) {
   const url = new URL(serviceUrl + 'getegrid/json/');
   url.searchParams.append('EN', '' + coord[0] + ',' + coord[1]);
   url.searchParams.append('_dc', new Date().getTime());
-  return fetch(url.toString()).then((response) => response.json());
+  return new Promise((resolve, reject) => {
+    fetch(url.toString()).then((response) => {
+      if (response.status === 204 || response.status >= 400) {
+        reject(response);
+      }
+      else {
+        resolve(response.json());
+      }
+    });
+  });
 };
---
+++ oereb_client/static/src/util/error.js
@@ -0,0 +1,6 @@
+export class NoDataError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'NoDataError';
+  }
+}
