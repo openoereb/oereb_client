dc243e812ebf50d11e575b5f4b7cacfce88afcc6
Karsten Deininger <karsten.deininger@bl.ch>
2023-12-05 12:11:08 +0000

    Add documentation

---
+++ oereb_client/static/src/component/message/message.stories.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import OerebMessage from "./message";
+import { showError, showWarning } from "../../reducer/message";
+import { useDispatch } from "react-redux";
+
+export default {
+  title: 'API Reference/Component/Message',
+  component: OerebMessage
+};
+
+export const Message = () => {
+  const dispatch = useDispatch();
+
+  const warning = () => {
+    dispatch(showWarning('This is a warning message example.'));
+  };
+
+  const error = () => {
+    dispatch(showError('This is an error message example.'));
+  };
+
+  return (
+    <div style={{'min-height': '300px'}}>
+      <button type="button" className="btn btn-primary me-3" onClick={warning}>Show warning</button>
+      <button type="button" className="btn btn-primary me-3" onClick={error}>Show error</button>
+      <OerebMessage />
+    </div>
+  )
+};
